a8d205e7f83e8d3d3f359a54a256b552cbb68bd0
Edward <edwaramor9@hotmail.com>
2018-02-04 10:04:58 -0600

    cree el repositorio

---
+++ learnJquey.js
@@ -0,0 +1,13 @@
+function res(){
+var nacimiento = document.getElementById('ano_nacimiento');
+var resultado = document.getElementById('resultado');
+edadActual=2017-nacimiento;
+
+resultado.innerHTML = parseInt(edadActual);
+}
+
+
+
+
+
+
